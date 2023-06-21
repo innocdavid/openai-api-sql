8e0f03217f2129329894fff104455f268671c26c
Banja David Lotigo <innocdavid@gmail.com>
2023-06-21 22:34:33 +0300

    feature: Add post endpoint for queries to openai api

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import generateAPIResponse from './generate.js';
 
 const app = express();
 
 // MIDDLEWARE
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || 'http://localhost';
 
-// route
+// get request
 app.get('/api/', (req, res) => {
-  res.status(200).json({ msg: 'Hello from the api' });
+  res.status(200).json({ response: 'Hello from the api' });
+});
+
+// post request
+app.post('/api/generate', async (req, res) => {
+  const queryDescription = req.body.queryDescription;
+  try {
+    const sqlQuery = await generateAPIResponse(queryDescription);
+    res.status(200).json({ response: sqlQuery });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(PORT, () => { console.log(`server listening on ${HOST}:${PORT}`) });
